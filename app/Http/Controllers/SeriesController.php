b2f780555d968e108027537e2d402a92d8f0468a
Emerson Schafhauser <emerson.scha@gmail.com>
2023-07-18 22:01:19 -0300

    DB Facade

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,22 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
-            'The Witcher',
-            'The Legend of Vox Machina',
-            'The Lord of the Rings',
-            'Grey\'s Anatomy'
-        ];
+        $series = DB::select('select nome from series');
 
         return view('series.index')->with('series',$series);
     }
 
     public function create() {
         return view('series.create');
+    }
+
+    public function store(Request $request) {
+        $nomeSerie = $request->input('nome');
+        if (DB::insert('insert into series (nome) values (?)', [$nomeSerie])) {
+            return "OK";
+        } else {
+            return "Deu erro";
+        }
     }
 }
